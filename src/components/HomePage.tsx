f7645d31829fa537d4d6fb6a5f9721fd2ffc297e
Xavier <facqueur.xavier@gmail.com>
2021-11-22 00:55:24 +0100

    Update HomeScreen images loaded

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import styles from "./HomePage.module.css";
 
 function HomePage() {
     return (
-        <div>
-            Home Page
+        <div className={styles.container}>
+            <section className={styles.section}>
+                <img id={styles.stars} src="landing-page/stars.png"/>
+                <img id={styles.moon} src="landing-page/moon.png"/>
+                <img id={styles.backWind} src="landing-page/back-wind.png"/>
+                <img id={styles.mountain} src="landing-page/mountain.png"/>
+                <img id={styles.frontWind} src="landing-page/front-wind.png"/>
+                <img id={styles.front} src="landing-page/front.png"/>
+                <img id={styles.frontParticles} src="landing-page/front-particle.png"/>
+            </section>
         </div>
     )
 }
